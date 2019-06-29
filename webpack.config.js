7d100e5d2d4f180a930cdae1dcefcf38850fa32e
Matteo Dema <matteo07@github.com>
2019-06-29 19:47:39 +0200

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path');
+
+const isDevelopment = true;
 
 module.exports = {
   entry: './src/index.js',
@@ -20,7 +23,48 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              camelCase: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    })
+  ]
 };
